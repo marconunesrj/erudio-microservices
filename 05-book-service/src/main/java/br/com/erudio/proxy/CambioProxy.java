f3a30caa73b11d0bb92ed62f5af409e1152721f7
marconunesrj <marconunes.rj@hotmail.com>
2024-03-07 13:48:24 -0300

    Inclusao do Eureka

--- 05-book-service/src/main/java/br/com/erudio/proxy/CambioProxy.java
+++ 05-book-service/src/main/java/br/com/erudio/proxy/CambioProxy.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.com.erudio.response.Cambio;
 
 //@FeignClient(name = "cambio-service", url = "localhost:8000,localhost:8001,localhost:8002")
-@FeignClient(name = "cambio-service", url = "localhost:8000")
+//@FeignClient(name = "cambio-service", url = "localhost:8000")
+@FeignClient(name = "cambio-service")  // url controlada pelo Eureka
 public interface CambioProxy {
 	
 	@GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
