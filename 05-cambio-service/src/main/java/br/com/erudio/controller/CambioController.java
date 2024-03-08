9ff93e0718d9441556358b3b77576ce5ae056634
marconunesrj <marconunes.rj@hotmail.com>
2024-03-08 14:05:10 -0300

    Implantacao Zipkin

--- 05-cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
+++ 05-cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
@@ -3,6 +3,8 @@ package br.com.erudio.controller;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +22,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("cambio-service")
 public class CambioController {
 	
-	@Autowired
+    private Logger logger = LoggerFactory.getLogger(CambioController.class);
+
+    @Autowired
 	private Environment environment;
 	
 	@Autowired
@@ -34,6 +38,7 @@ public class CambioController {
 			@PathVariable("to") String to
 			) {
 
+        logger.info("getCambio is called with -> {}, {} and {}", amount, from, to);
 		var cambio = repository.findByFromAndTo(from, to);
 		if (cambio == null) throw new RuntimeException("Currency Unsupported");
 		
---
+++ subir_openzipkin_docker.bat
@@ -0,0 +1,3 @@
+Rem Eu Abaixei esta
+docker run -it --name zipkin -p 9411:9411 openzipkin/zipkin
+Rem docker run -it --name zipkin -p 9411:9411 openzipkin/zipkin:2.23.2
